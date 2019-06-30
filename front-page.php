41e670c2a8e70a4878e3b7288c3b55016b235673
ErikRobles <erikjames69@hotmail.com>
2019-06-30 13:59:15 -0500

    created robot.txt and acf-json file for version control

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Template Name: Front-page
+ */
+
+$context = Timber::context();
+
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
