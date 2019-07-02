3b2f513526aaf3ec44bc8e6b4bb3f4002f6acae2
ErikRobles <erikjames69@hotmail.com>
2019-07-01 21:00:25 -0500

    started sass work

--- page.php
+++ page.php
@@ -21,8 +21,7 @@
  * @since    Timber 0.1
  */
 
-$context = Timber::context();
-
-$timber_post = new Timber\Post();
-$context['post'] = $timber_post;
-Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
