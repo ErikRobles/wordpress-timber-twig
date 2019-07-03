bec8a0010ed387a491c3e2eb2fe76d4fd6521e22
ErikRobles <erikjames69@hotmail.com>
2019-07-03 11:59:47 -0500

    unable to render single.twig

--- single.php
+++ single.php
@@ -9,13 +9,18 @@
  * @since    Timber 0.1
  */
 
-$context = Timber::get_context();
-$post = Timber::query_post();
-$context['post'] = $post;
-$context['comment_form'] = TimberHelper::get_comment_form();
+// $context = Timber::get_context();
+// $post = Timber::query_post();
+// $context['post'] = $post;
+// $context['comment_form'] = TimberHelper::get_comment_form();
 
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
-} else {
-	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
-}
+// if ( post_password_required( $post->ID ) ) {
+// 	Timber::render( 'single-password.twig', $context );
+// } else {
+// 	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+// }
+
+$data = Timber::get_context(); 
+$data['foo'] = 'Bar!';
+$data['post'] = new TimberPost();
+Timber::render('single.twig', $data);
